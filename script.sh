baaca0c50eb69276a681350fe1937e91c8cae3f7
Matteo Giorgi <mattegiorgi@gmail.com>
2019-09-24 16:30:51 +0200

    comment added

--- script.sh
+++ script.sh
@@ -1,2 +1,6 @@
+#!/usr/bin/env bash
+
+# Script Bash usefull to avoid security problems in the firfox
+# You can run this script directly from the shell
 python -m SimpleHTTPServer 8000 &
 firefox localhost:8000
