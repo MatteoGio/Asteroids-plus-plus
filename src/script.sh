b94fe60ce120768db0e38fc4ca0187e94f1bfeca
matteo <matteo.giorgi@protonmail.com>
2020-05-15 12:57:18 +0200

    changed and changed back

--- src/script.sh
+++ src/script.sh
@@ -2,5 +2,6 @@
 
 # Script Bash usefull to avoid security problems in the firfox
 # You can run this script directly from the shell
-python -m SimpleHTTPServer 8000 &
+python3 -m http.server 8000 &
+#python3 -m SimpleHTTPServer 8000 &
 firefox localhost:8000
